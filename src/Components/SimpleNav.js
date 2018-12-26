86bc42a92c9b3c78a06f44ab23483d59903d1d19
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-12-26 10:07:07 -0800

    changed file name

---
+++ src/Components/SimpleNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const SimpleNav = (props)=>{
+  return(
+    <div className="nav-div">
+        <a href="/"><span className="nav-span">HOME</span></a>
+        <a href="/upload"><span className="nav-span">UPLOAD</span></a>
+        <a href="/favorites"><span className="nav-span">FAVORITES</span></a>
+        <a href="/random"><span className="nav-span">RANDOM</span></a>
+    </div>
+  )
+}
+export default SimpleNav;
