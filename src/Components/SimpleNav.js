e631bf0e9448492138016d5f174241fbc7fdb58b
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-12-26 15:22:10 -0800

    added giphy logo to nav

--- src/Components/SimpleNav.js
+++ src/Components/SimpleNav.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import giphy_logo from '../giphy_logo.png'
 
 const SimpleNav = (props)=>{
   return(
     <div className="nav-div">
+        <a href="/"><img src={giphy_logo} className="nav-img" alt="giphy logo" /></a>
         <a href="/"><span className="nav-span">HOME</span></a>
         <a href="/upload"><span className="nav-span">UPLOAD</span></a>
         <a href="/favorites"><span className="nav-span">FAVORITES</span></a>
