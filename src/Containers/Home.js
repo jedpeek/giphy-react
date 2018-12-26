d9b0cd1b0fcd269b30dabafb85477f225eb14d1a
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-12-26 10:45:43 -0800

    added search form to home container

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 import dummy_data from '../dummy_data'
 import {Container, Row} from 'reactstrap'
 import GifCard from '../Components/GifCard'
+import SearchForm from '../Components/SearchForm'
+const api_key = process.env.GIPHY_KEY || 'XeV04VURnwNCs7nYczgfCQ3bl7udAXiX'
+
 class Home extends Component {
   state = {
     gifs:[],
@@ -18,6 +21,17 @@ class Home extends Component {
     }, 3000)
     this.setState({gifs: dummy_data.data})
   }
+// Search Giphy based on Query
+  giphySearch = (event)=>{
+    event.preventDefault();
+    this.setState({loaded: false})
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.query}&api_key=${api_key}`)
+    .then(response => this.setState({gifs: response.data.data}))
+    .then(()=> this.setState({loaded: true}))
+    .catch(error => console.log(error));
+  }
+// Handle form change
+  handleChange = (event)=> this.setState({[event.target.name]: event.target.value});
 
   componentDidMount(){
     this.giphyData()
@@ -27,6 +41,13 @@ class Home extends Component {
     return (
         <Container fluid>
           <Row className='last'>
+          <SearchForm
+            name='query'
+            gifs={this.state.gifs}
+            loaded={this.state.loaded}
+            giphySearch={this.giphySearch}
+            handleChange={this.handleChange}
+            />
             { gifs.map(gif => <GifCard gif={gif} key={gif.id} loaded={loaded} />)}
           </Row>
         </Container>
